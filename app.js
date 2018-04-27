f021b848dd04dc1dfa3b9a2dfa05ffa4f230e1b9
Ana Silva <ananunesdasilva@gmail.com>
2018-04-27 17:09:28 +0100

    App structure

---
+++ app.js
@@ -0,0 +1,17 @@
+var budgetController = (function() {
+
+// TODO
+
+})();
+
+var UIController = (function() {
+
+// TODO
+
+})();
+
+var controller = (function(budgetCtrl, UICtrl) {
+
+// TODO
+
+})(budgetController, UIController);
